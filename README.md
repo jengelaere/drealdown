1e5c0b0e4f94e8877a9ce4eb18ee6fd8b2060dcb
statnmap <statnmap@users.noreply.github.com>
2019-05-16 23:32:44 +0200

    init drealdown

---
+++ README.md
@@ -0,0 +1,42 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# drealdown
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+This is package {drealdown}: Templates for html outputs. You are using
+version 0.0.0.9000.
+
+## Installation
+
+The list of dependencies required to install this package is:
+
+    #> [1] "bookdown"  "rmarkdown"
+
+To install the package, you can run the following script
+
+``` r
+# install.packages("remotes")
+remotes::install_git(url = "https://git.thinkr.fr/ThinkR/Missions/drealdown")
+```
+
+## Templates
+
+### Voir tous les exemples inclus
+
+``` r
+examples <- system.file("examples", package = "drealdown")
+browseURL(examples)
+```
+
+### Fonctions incluses
+
+  - Markdown classique de type `rmarkown::html_document()` :
+    `drealdown::html_mark()`
+  - Markdown avancé avec bookdown de type `bookdown::html_document2()` :
+    `drealdown::html2_book()`
+  - Markdown pour vignette de type `rmarkdown::html_vignette()` :
+    `drealdown::html__vignette()`
---
+++ devstuff_history.R
@@ -0,0 +1,118 @@
+# Hide this file from build
+usethis::use_build_ignore("devstuff_history.R")
+usethis::use_build_ignore("inst/dev")
+usethis::use_build_ignore("rsconnect")
+usethis::use_git_ignore("docs/")
+usethis::use_git_ignore("rsconnect/")
+# usethis::create_package(".")
+
+# description ----
+library(desc)
+unlink("DESCRIPTION")
+my_desc <- description$new("!new")
+my_desc$set_version("0.0.0.9000")
+my_desc$set(Package = "drealdown")
+my_desc$set(Title = "Templates for html outputs")
+my_desc$set(Description = "Templates for rmarkdown, bookdown and flexdashboard.")
+my_desc$set("Authors@R",
+            'c(
+  person("Sebastien", "Rochette", email = "sebastien@thinkr.fr", role = c("aut", "cre"), comment = c(ORCID = "0000-0002-1565-9313")),
+  person(given = "ThinkR", role = "cph")
+)')
+my_desc$set("VignetteBuilder", "knitr")
+my_desc$del("Maintainer")
+my_desc$del("URL")
+my_desc$del("BugReports")
+my_desc$write(file = "DESCRIPTION")
+
+# Licence ----
+usethis::use_mit_license("ThinkR")
+# usethis::use_gpl3_license("ThinkR")
+
+# Pipe ----
+usethis::use_pipe()
+
+# Package quality ----
+
+# _Tests
+usethis::use_testthat()
+usethis::use_test("app")
+
+# _CI
+thinkridentity::use_gitlab_ci(image = "thinkr/runnerci", upgrade = "never")
+# usethis::use_travis()
+# usethis::use_appveyor()
+# usethis::use_coverage()
+
+# _rhub
+# rhub::check_for_cran()
+
+
+# Documentation ----
+usethis::use_roxygen_md()
+# _Readme
+usethis::use_readme_rmd()
+# _News
+usethis::use_news_md()
+# _Vignette
+thinkridentity::create_vignette_thinkr("aa-data-exploration")
+usethis::use_vignette("ab-model")
+devtools::build_vignettes()
+
+
+# _Book
+# thinkridentity::install_git_with_pwd(repo = "ThinkR/visualidentity", username, password, host = "git.thinkr.fr")
+visualidentity::create_book("inst/report", clean = TRUE)
+visualidentity::open_guide_function()
+devtools::document()
+visualidentity::build_book(clean_rmd = TRUE, clean = TRUE)
+# pkg::open_guide()
+
+# _Pkgdown
+visualidentity::build_pkgdown(
+  # lazy = TRUE,
+  yml = system.file("pkgdown/_pkgdown.yml", package = "thinkridentity"),
+  favicon = system.file("pkgdown/favicon.ico", package = "thinkridentity"),
+  move = TRUE, clean_before = TRUE, clean_after = TRUE
+)
+
+visualidentity::open_pkgdown_function(path = "docs")
+# pkg::open_pkgdown()
+
+## __ deploy on rsconnect
+usethis::use_git_ignore("docs/rsconnect")
+usethis::use_git_ignore("inst/docs/rsconnect")
+usethis::use_git_ignore("rsconnect")
+
+rsconnect::accounts()
+account_name <- rstudioapi::showPrompt("Rsconnect account", "Please enter your username:", "name")
+account_server <- rstudioapi::showPrompt("Rsconnect server", "Please enter your server name:", "1.1.1.1")
+origwd <- setwd("inst/docs")
+rsconnect::deployApp(
+  ".",                       # the directory containing the content
+  appFiles = list.files(".", recursive = TRUE), # the list of files to include as dependencies (all of them)
+  appPrimaryDoc = "index.html",                 # the primary file
+  appName = "appname",                   # name of the endpoint (unique to your account on Connect)
+  appTitle = "appname",                  # display name for the content
+  account = account_name,                # your Connect username
+  server = account_server                    # the Connect server, see rsconnect::accounts()
+)
+setwd(origwd)
+
+# Dependencies ----
+# devtools::install_github("ThinkR-open/attachment")
+attachment::att_to_description()
+# attachment::att_to_description(extra.suggests = c("pkgdown"))
+# attachment::create_dependencies_file()
+
+# Description and Bibliography
+visualidentity::create_pkg_desc_file(out.dir = "inst", source = c("git"),
+                                     to = "markdown", url = "https://git.thinkr.fr/ThinkR/Missions/drealdown")
+thinkridentity::create_pkg_biblio_file_thinkr()
+
+# Utils for dev ----
+devtools::install(upgrade = "never")
+# devtools::load_all()
+devtools::check(vignettes = TRUE)
+# ascii
+stringi::stri_trans_general("é", "hex")
